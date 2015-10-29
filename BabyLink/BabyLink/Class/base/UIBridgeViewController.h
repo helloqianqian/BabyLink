91d0f7e323995fa97225face73b33570c461cd07
huangqian <huangqian@ledoing.cn>
2015-10-29 10:41:27 +0800

    添加结构

---
+++ BabyLink/BabyLink/Class/base/UIBridgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIBridgeViewController.h
+//  BabyLink
+//
+//  Created by 黄倩 on 15/10/27.
+//  Copyright © 2015年 黄倩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBridgeViewController : UIViewController
+
+@end
---
+++ BabyLink/BabyLink/globalParams/NSUserInfoObject.swift
@@ -0,0 +1,13 @@
+//
+//  NSUserInfoObject.swift
+//  BabyLink
+//
+//  Created by 黄倩 on 15/10/27.
+//  Copyright © 2015年 黄倩. All rights reserved.
+//
+
+import UIKit
+
+class NSUserInfoObject: NSObject {
+
+}
